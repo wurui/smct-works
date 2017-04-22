9c1e3e491de57f7cb155f23b9d551c05626d6448
wurui <wurui.wr@gmail.com>
2017-04-22 11:57:17 +0800

    fir st

--- asset/index.js
+++ asset/index.js
@@ -1,7 +1,76 @@
-define([],function(){
-  return {
-    init:function($mod){
+define(['zepto', 'mustache'], function (undef, Mustache) {
+    var timeformat = function (d) {
+        if (typeof d != 'object') {
+            d = new Date(d);
+        }
+        var prefix0 = function (n) {
+            return (n / 100).toFixed(2).substr(2)
+        };
+        return [d.getFullYear(), prefix0(d.getMonth() + 1), prefix0(d.getDate())].join('-') + ' ' + [d.getHours(), prefix0(d.getMinutes()), prefix0(d.getSeconds())].join(':')
+    };
 
+    return {
+        init: function ($mod) {
+            var tpl = $('.J_tpl').html();
+            $.getJSON('http://www.shaomachetie.com/smct/getbuilds?callback=?', function (r) {
+                var list = r.data;
+                var totalfee = 0;
+
+                if (list.length) {
+                    for (var i = 0, n; n = list[i++];) {
+                        totalfee += n.price;
+                        n.time = timeformat(n.cts)
+                    }
+                    //r.data=list.slice(0,4)
+                    r.extcls = list % 2 ? '' : 'fix-flex';
+                    r.showtopbar = r.data.length > 4;
+                    $mod.html(Mustache.render(tpl, r));
+                } else {
+                    $mod.html('<div class="empty"><i class="iconfont" style="font-size:50px;color:#ccc;">&#xe631;</i>&nbsp;&nbsp;<br/>作品栏是空的,赶紧去定制一个你喜欢的车贴吧~<br/><a href="'+$mod.attr('data-shopcart')+'">开始定制 &raquo;</a><br/><br/></div>')
+                }
+
+
+                var btDel = $('.J_del', $mod).on('click', function () {
+                        if (confirm('确认删除?')) {
+                            var ids=[];
+                            $('.J_list>.selected', $mod).each(function(){
+                                ids.push(this.getAttribute('data-id'))
+                            })
+
+                            $.getJSON('http://www.shaomachetie.com/smct/delbuild?ids='+ids.join(',')+'&callback=?',function(r){
+                                if(r.code==0) {
+                                    $('.J_list>.selected', $mod).remove();
+                                }else{
+                                    alert(r.error)
+                                }
+                            });
+                        }
+                    }),
+                    btOrder = $('.J_order', $mod).on('click', function () {
+                        var ids=[];
+                        $('.J_list>.selected', $mod).each(function(){
+                            ids.push(this.getAttribute('data-id'))
+                        })
+
+                        location.href=$mod.attr('data-shopcart')+'?bids='+ids.join(',')
+                    });
+                $($mod).on('click', '.snapshot', function (e) {
+                    $(this).toggleClass('selected');
+                    var ck = $('input[type="checkbox"]', this)[0];
+                    ck.checked = $(this).hasClass('selected');
+                    var len = $('.J_list>.selected', $mod).length
+                    $('.J_count', $mod).html(len)
+
+                    if (len) {
+                        btDel.removeAttr('disabled')
+                        btOrder.removeAttr('disabled')
+                    } else {
+                        btDel.attr('disabled', 'disabled')
+                        btOrder.attr('disabled', 'disabled')
+                    }
+
+                });
+            });
+        }
     }
-  }
 })
--- mod.xsl
+++ mod.xsl
@@ -1,10 +1,65 @@
 <xsl:stylesheet version="1.0" xmlns:xsl="http://www.w3.org/1999/XSL/Transform" xmlns:oxm="https://www.openxsl.com">
     <xsl:template match="/root" name="wurui.smct-works">
+        <xsl:param name="actionShopcart"></xsl:param>
+        <xsl:param name="actionBuild"></xsl:param>
         <!-- className 'J_OXMod' required  -->
-        <div class="J_OXMod oxmod-smct-works" ox-mod="smct-works">
-            <h1>
-                This is mod smct-works;
-            </h1>
+        <div class="J_OXMod oxmod-smct-works" ox-mod="smct-works" data-shopcart="{$actionShopcart}" data-build="{$actionBuild}">
+            <div class="loading">加载中...</div>
+
         </div>
+
+        <script type="text/tpl" class="J_tpl"><![CDATA[
+
+            {{#showtopbar}}
+            <div class="listbar">
+                <span>
+                    &nbsp;&nbsp;已选中 <span class="J_count">0</span>/{{data.length}}
+                    <button class="bt-del J_del" disabled="disabled">删除已选</button>
+                </span>
+                <span>
+                    <button class="bt-order J_order" disabled="disabled">结算</button>
+                </span>
+            </div>
+            {{/showtopbar}}
+            <div class="list J_list {{extcls}}">
+            {{#data}}
+            <div class="snapshot" data-id="{{_id}}" title="点击可选中">
+                <div class="snapshot-op">
+                    <p>时间:{{time}}</p>
+                    <span class="status status-{{status}}"></span>
+                    <p class="radio">
+                        <input type="checkbox" name="preview" value="{{_id}}"/>
+                    </p>
+                </div>
+                <div class="preview bgcolor-{{setting.bgcolor}}"">
+                    <div class="card-header">{{setting.text1}}</div>
+                    <div class="card-body tpl tpl-{{setting.tpl}}">
+                        <div class="central">
+                            {{#setting.carlogo}}<img src="http://www.shaomachetie.com/static/smct/img/carlogo/{{setting.carlogo}}.jpg"/>{{/setting.carlogo}}
+                        </div>
+                        <img src="http://i.oxm1.cc/uploads/git/wurui/img/2ahkwkkveTj1rgh0ueRlcquA5vz-1000.png" class="qrcode"/>
+
+                    </div>
+                    <div class="card-footer">
+                        <span>{{setting.text2}}</span>
+                    </div>
+                </div>
+            </div>
+            {{/data}}
+            </div>
+            <div class="listbar">
+                <span>
+                    &nbsp;&nbsp;已选中 <span class="J_count">0</span>/{{data.length}}
+                    <button class="bt-del J_del" disabled="disabled">删除己选</button>
+                </span>
+                <span>
+                    <button class="bt-order J_order" disabled="disabled">结算</button>
+                </span>
+            </div>
+
+
+
+        ]]>
+        </script>
     </xsl:template>
 </xsl:stylesheet>

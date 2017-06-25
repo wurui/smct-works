3bbecadf31f6a61c2b2311bcf9274aa59294b8af
wurui <wurui.wr@gmail.com>
2017-06-25 12:05:06 +0800

    fix bug and oxjs.confirm

--- asset/index.js
+++ asset/index.js
@@ -1,4 +1,4 @@
-define(['zepto', 'mustache'], function (undef, Mustache) {
+define(['zepto', 'mustache','oxjs'], function (undef, Mustache,OXJS) {
     var timeformat = function (d) {
         if (typeof d != 'object') {
             d = new Date(d);
@@ -24,48 +24,56 @@ define(['zepto', 'mustache'], function (undef, Mustache) {
                     //r.data=list.slice(0,4)
                     r.extcls = list % 2 ? '' : 'fix-flex';
                     r.showtopbar = r.data.length > 4;
-                    r.fullcarlogo=function(){
+                    r.fullcarlogo = function () {
 
-                        var str=''
-                        if(/\d+/.test(this)){
-                            str='cars/'+this+'.png'
-                        }else{
-                            str='carlogo/'+this+'.jpg'
+                        var str = ''
+                        if (/\d+/.test(this)) {
+                            str = 'cars/' + this + '.png'
+                        } else {
+                            str = 'carlogo/' + this + '.jpg'
                         }
-                        return 'http://v.oxm1.cc/'+str
+                        return 'http://v.oxm1.cc/' + str
                     }
                     $mod.html(Mustache.render(tpl, r));
                 } else {
-                    $mod.html('<div class="empty"><i class="iconfont" style="font-size:50px;color:#ccc;">&#xe631;</i>&nbsp;&nbsp;<br/>作品栏是空的,赶紧去定制一个你喜欢的车贴吧~<br/><a href="'+$mod.attr('data-build')+'">开始定制 &raquo;</a><br/><br/></div>')
+                    $mod.html('<div class="empty"><i class="iconfont" style="font-size:50px;color:#ccc;">&#xe631;</i>&nbsp;&nbsp;<br/>作品栏是空的,赶紧去定制一个你喜欢的车贴吧~<br/><a href="' + $mod.attr('data-build') + '">开始定制 &raquo;</a><br/><br/></div>')
                 }
 
 
                 var btDel = $('.J_del', $mod).on('click', function () {
-                        if (confirm('确认删除?')) {
-                            var ids=[];
-                            $('.J_list>.selected', $mod).each(function(){
-                                ids.push(this.getAttribute('data-id'))
-                            })
+                        OXJS.confirm('确认删除?',function(re){
+                            if (re) {
+                                var ids = [];
+                                $('.J_list>.selected', $mod).each(function () {
+                                    ids.push(this.getAttribute('data-id'))
+                                })
 
-                            $.getJSON('//www.shaomachetie.com/smct/delbuild?ids='+ids.join(',')+'&callback=?',function(r){
-                                if(r.code==0) {
-                                    $('.J_list>.selected', $mod).remove();
-                                    location.reload()
-                                }else{
-                                    alert(r.error)
-                                }
-                            });
-                        }
+                                $.getJSON('//www.shaomachetie.com/smct/delbuild?ids=' + ids.join(',') + '&callback=?', function (r) {
+                                    if (r.code == 0) {
+                                        $('.J_list>.selected', $mod).remove();
+                                        location.reload()
+                                    } else {
+                                        alert(r.error)
+                                    }
+                                });
+                            }
+                        })
+
                     }),
                     btOrder = $('.J_order', $mod).on('click', function () {
-                        var ids=[];
-                        $('.J_list>.selected', $mod).each(function(){
+                        var ids = [];
+                        $('.J_list>.selected', $mod).each(function () {
                             ids.push(this.getAttribute('data-id'))
                         })
 
-                        location.href=$mod.attr('data-shopcart')+'?bids='+ids.join(',')
+                        location.href = $mod.attr('data-shopcart') + '?bids=' + ids.join(',')
                     });
-                $($mod).on('tap', '.snapshot', function (e) {
+                var lastClickTS=0;
+                $mod.on('tap', '.snapshot', function (e) {
+                    if(Date.now()-lastClickTS < 100){
+                        return false
+                    }
+                    lastClickTS=Date.now();
                     $(this).toggleClass('selected');
                     var ck = $('input[type="checkbox"]', this)[0];
                     ck.checked = $(this).hasClass('selected');
@@ -79,6 +87,7 @@ define(['zepto', 'mustache'], function (undef, Mustache) {
                         btDel.attr('disabled', 'disabled')
                         btOrder.attr('disabled', 'disabled')
                     }
+                    return false
 
                 });
             });
